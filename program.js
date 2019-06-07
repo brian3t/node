3f73c1acd691d39aa31ea7e365e70e719c558e71
brian3t <ngxtri@gmail.com>
2019-06-06 21:26:37 -0700

    all done

--- program.js
+++ program.js
@@ -17,22 +17,49 @@ function zero_fill(input) {
 
 let server = http.createServer(
     (req, res) => {
-        if (req.method !== 'GET'){
+        let unixtime = Math.floor(new Date())
+
+        if (req.method !== 'GET') {
             return res.end('oh no GET only')
         }
-        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.writeHead(200, {'Content-Type': 'application/json'})
 
-        if (req.url.slice(0,4) !== '/api'){
+        let url_parts = url.parse(req.url, true).query
+        if (! url_parts.iso) {
+            return res.end('I need iso')
+        }
+        let iso_ts = url_parts.iso
+        let date = new Date(iso_ts)
+        let url_pathname = url.parse(req.url, true).pathname
+        let url_path_parts = url_pathname.split('/')
+        url_path_parts.shift() //remove preceding slash
+        let url_path_part_1 = url_path_parts.shift()
+        if (url_path_part_1 !== 'api') {
             return res.end('We need /api')
         }
-        let url_parts = url.parse(req.url, true).query
-
-
-        req.pipe(map(
-            (chunk) => {
-                return chunk.toString().toUpperCase()
+        let url_path_part_2 = url_path_parts.shift()
+        if (! url_path_part_2) {
+            return res.end('malformed url')
+        }
+        switch (url_path_part_2) {
+            case 'parsetime': {
+                let hour = date.getHours()
+                let reply_object = {
+                    hour: hour,
+                    minute: date.getMinutes(),
+                    second: date.getSeconds()
+                }
+                return res.end(JSON.stringify(reply_object))
             }
-        )).pipe(res)
+            case 'unixtime': {
+                let date = new Date()
+                return res.end(JSON.stringify({
+                    unixtime: date.getTime()
+                }))
+            }
+            default:
+                return res.end('')
+        }
 
         return res
     })
