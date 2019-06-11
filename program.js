929bda028e3b923dbb3ccd32765299d2426267fb
brian3t <ngxtri@gmail.com>
2019-06-11 14:32:23 -0700

    npm i functional-javascript-workshop ./node_modules/functional-javascript-workshop/functional-javascript.js ./node_modules/functional-javascript-workshop/functional-javascript.js verify program.js

--- program.js
+++ program.js
@@ -1,68 +1,10 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
-const http = require('http');
-const bl = require('bl')
-const map = require('through2-map')
-const url = require('url')
-
-const net = require('net')
-let port_num = process.argv[2]
-
-function zero_fill(input) {
-    return String(input).padStart(2, '0')
+function getShortMessages(message_objects) {
+    let messages_only = message_objects.map(message => message.message)
+    return messages_only.filter(message => message.split('').length < 50)
 }
 
 
-let server = http.createServer(
-    (req, res) => {
-        let unixtime = Math.floor(new Date())
-
-        if (req.method !== 'GET') {
-            return res.end('oh no GET only')
-        }
-        res.writeHead(200, {'Content-Type': 'application/json'})
-
-        let url_parts = url.parse(req.url, true).query
-        if (! url_parts.iso) {
-            return res.end('I need iso')
-        }
-        let iso_ts = url_parts.iso
-        let date = new Date(iso_ts)
-        let url_pathname = url.parse(req.url, true).pathname
-        let url_path_parts = url_pathname.split('/')
-        url_path_parts.shift() //remove preceding slash
-        let url_path_part_1 = url_path_parts.shift()
-        if (url_path_part_1 !== 'api') {
-            return res.end('We need /api')
-        }
-        let url_path_part_2 = url_path_parts.shift()
-        if (! url_path_part_2) {
-            return res.end('malformed url')
-        }
-        switch (url_path_part_2) {
-            case 'parsetime': {
-                let hour = date.getHours()
-                let reply_object = {
-                    hour: hour,
-                    minute: date.getMinutes(),
-                    second: date.getSeconds()
-                }
-                return res.end(JSON.stringify(reply_object))
-            }
-            case 'unixtime': {
-                let date = new Date()
-                return res.end(JSON.stringify({
-                    unixtime: date.getTime()
-                }))
-            }
-            default:
-                return res.end('')
-        }
-
-        return res
-    })
-
-server.listen(port_num)
-// server.listen(8000)
+// Do not remove the line below
+module.exports = getShortMessages
---
+++ testprogram.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let program = require('./program')
+
+let input = [
+    {name: 'a', message: 'Tempor quis esse consequat sunt ea eiusmod.'},
+    {type: 'b', message: 'Id culpa ad proident ad nulla laborum incididunt.'},
+    {count: 4, message: 'Ullamco in ea et ad anim anim ullamco est.'},
+    {is_active: true, message: 'Est ut irure irure nisi.'}]
+
+console.log(program(input))
