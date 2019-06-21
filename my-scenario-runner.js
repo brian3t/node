eec3160827eac9906ac7160d6dae32deab979972
brian3t <ngxtri@gmail.com>
2019-06-21 12:59:51 -0700

    bugclinic all finished

--- my-scenario-runner.js
+++ my-scenario-runner.js
@@ -1,16 +1,9 @@
 "use strict";
-var resolve = require("path").resolve;
-
-const bunyan = require('bunyan')
-const stackup = require("stackup");
-const util = require('util')
-const tap = require('tap')
-var log = bunyan.createLogger({name: "sample"});
-
-var scenario = require(resolve(process.cwd(), process.argv[2]));
-
-scenario(tap);
-
-
-let a = 1
-// console.log(a)
+//  scan-server.js
+var createServer = require("http").createServer;
+var server = createServer(function (req, res) {
+    res.end("hello");
+});
+server.listen(9876, function () {
+    console.log("listening");
+});
