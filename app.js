8d6b611a4aef54c931132d7b58f04ea5bcf0c371
Brian Nguyen <ngxtri@gmail.com>
2019-11-10 08:21:58 -0800

    serving favicon

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
+const favicon = require('serve-favicon')
+const path = require('path')
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 app.get('/url2', (req, res, next)=>{
     return res.end('hiyabcded')
 })
 
-app.listen(80,'nodelocal')
+app.get('/', (req, res, next)=>{
+    return res.end('root here')
+})
+
+app.listen(3000,'nodelocal')
 
