c3496d09256248eb241a9161de512735a230e219
brian <trapok02>
2019-11-21 21:38:47 -0800

    page 46 static

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 const express = require('express')
 const ejs = require('ejs')
+const cors = require('cors'); // Use cors module for enable Cross-origin resource sharing
 const greet_middleware = require('./greet')
 const other = require('./other')
+
 const app = express()
+app.use(cors())
 const favicon = require('serve-favicon')
 const path = require('path')
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(express.static('public'));
 app.set('view engine', 'ejs')
 // app.set('view', 'src/views')
-app.use('/user', (req, res, next)=>{
+app.use('/user', (req, res, next) => {
     res.render('user', {supplies: ['cake', 'cookie', 'detox', 'beef'], title: 'user title here'})
+})
+app.get('/json', function (req, res){
+    var info = {
+        'string_value': 'StackOverflow'//,GoalKicker.com – Node.js Notes for Professionals 35
+        , 'number_value': 8476
+    }
+    res.status(200).json(info);
+// or
+    /* res.send(JSON.stringify({
+    string_value: 'StackOverflow',
+    number_value: 8476
+    })) */
+//you can add a status code to the json response
+    /* res.status(200).json(info) */
 })
 
 class GreetingService {
@@ -30,7 +48,7 @@ app.get('/', (req, res, next) => {
     return res.end('root here')
 })
 
-app.listen(3000, 'nodelocal')
+app.listen(80, 'nodelocal')
 
 
 function myFunction(){
