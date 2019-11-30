3e894ebc4b32f94626294768b6fd1408e7693808
Brian Nguyen <ngxtri@gmail.com>
2019-11-29 19:28:51 -0800

    page 55 handling POST request

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 const express = require('express')
 
 var app = express()
+app.get('/bananas', function (req, res, next){
+    getMember(function (err, member){
+        if (err) return next(err);
+// If there's no member, don't try to look
+// up data. Just go render the page now.
+        if (! member) return next('route');
+// Otherwise, call the next middleware and fetch
+// the member's data.
+        req.member = member;
+        next();
+    });
+}, function (req, res, next){
+    getMemberData(req.member, function (err, data){
+        if (err) return next(err);
+// If this member has no data, don't bother
+// parsing it. Just go render the page now.
+        if (! data) return next('route');
+// Otherwise, call the next middleware and parse
+// the member's data. THEN render the page.
+        req.member.data = data;
+        next();
+    });
+}, function (req, res, next){
+    req.member.parsedData = parseMemberData(req.member.data);
+    next();
+});
+app.get('/bananas', function (req, res, next){
+    renderBananas(req.member);
+});
 
-app.use('/wiki', (req, res, next)=>{
-    res.end('hello world')
-})
-
-app.use('/', (req,res,next)=>{
-    res.end(`hi I'm root`)
-})
+app.listen(3000)
