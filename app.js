89fc2e2df77d08dc5a0e2c8198e84a2ce4f3df9a
brian3t <ngxtri@gmail.com>
2019-11-29 21:07:42 -0800

    page 50 race condition

--- app.js
+++ app.js
@@ -1,34 +1,13 @@
 const express = require('express')
-
+const bodyparser = require('body-parser')
 var app = express()
-app.get('/bananas', function (req, res, next){
-    getMember(function (err, member){
-        if (err) return next(err);
-// If there's no member, don't try to look
-// up data. Just go render the page now.
-        if (! member) return next('route');
-// Otherwise, call the next middleware and fetch
-// the member's data.
-        req.member = member;
-        next();
-    });
-}, function (req, res, next){
-    getMemberData(req.member, function (err, data){
-        if (err) return next(err);
-// If this member has no data, don't bother
-// parsing it. Just go render the page now.
-        if (! data) return next('route');
-// Otherwise, call the next middleware and parse
-// the member's data. THEN render the page.
-        req.member.data = data;
-        next();
-    });
-}, function (req, res, next){
-    req.member.parsedData = parseMemberData(req.member.data);
-    next();
-});
-app.get('/bananas', function (req, res, next){
-    renderBananas(req.member);
-});
+const path = require('path')
+const fs = require('fs')
+
+app.use((req,res,next)=>{
+    fs.readFile(path.resolve(__dirname, 'views/user.ejs'), (err, binaryContent)=>{
+        res.end(`string sent was this hex string ` + binaryContent.toString('hex'))
+    })
+})
 
 app.listen(3000)
